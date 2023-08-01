1a5180f9a620aa60ed49104cd73c13c09f5111d9
Rana <113706935+RanaGaballah@users.noreply.github.com>
2023-08-01 19:32:32 +0300

    Update Maintenance_STG_API.py

--- Maintenance_STG_API.py
+++ Maintenance_STG_API.py
@@ -1,12 +1,13 @@
 import requests
 import json
-
+import os
 
 Login_URL = "https://beta.friendycar.com/api/login"
 Dashboard_URL = "https://beta.friendycar.com/maintenance-api/v1/dashboard"
 Upcoming_URL = "https://beta.friendycar.com/maintenance-api/v1/maintenances?status=Upcoming&page=1&per_page=10&date_filter_type"
 History_URL = "https://beta.friendycar.com/maintenance-api/v1/maintenances?status=History&page=1&per_page=10&date_filter_typee"
-
+email1 = os.environ.get('USERNAME')
+password1 = os.environ.get('PASSWORD')
 def DashBoard_API(URL):
     access_token = '51|sUA935JiWC0hKaoHWc0upN7h6QRf43GGVoVA4xMV'
     vheaders = {
@@ -61,8 +62,8 @@ def Upcoming_API(URL):
 
 def Login_API(URL):
     Login_Data = {
-        "email": "maintenance.portal@friendycar.com",
-        "password": "test1234"
+        "email": email,
+        "password": password
     }
 
     vheaders = {
